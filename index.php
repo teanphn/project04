a280f8bc382b4015ff290f04e35f0baf05f3b30c
teanphn <90377837+teanphn@users.noreply.github.com>
2021-09-26 22:57:47 +0700

    commit

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
+    
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <link rel="stylesheet" href="css/style.css">
@@ -77,9 +77,8 @@
     </section>
     <!-- Explore Foods Ends -->
 
-    <!-- Food Menu -->
-   <!-- fOOD MEnu Section Starts Here -->
-   <section class="food-menu">
+    <!-- Food Menu start -->
+    <section class="food-menu">
         <div class="container">
             <h2 class="text-center">Food Menu</h2>
 
@@ -188,7 +187,7 @@
 
             <div class="clearfix"></div>
 
-            
+
 
         </div>
 
@@ -196,33 +195,31 @@
             <a href="#">See All Foods</a>
         </p>
     </section>
-    <!-- fOOD Menu Section Ends Here -->
+    <!-- Food menu ends -->
 
-    <!-- social Section Starts Here -->
+    <!-- Footer Starts -->
     <section class="social">
         <div class="container text-center">
             <ul>
                 <li>
-                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png" /></a>
                 </li>
                 <li>
-                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" /></a>
                 </li>
                 <li>
-                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png" /></a>
                 </li>
             </ul>
         </div>
     </section>
-    <!-- social Section Ends Here -->
 
-    <!-- footer Section Starts Here -->
     <section class="footer">
         <div class="container text-center">
             <p>All rights reserved. Designed By <a href="#">#</a></p>
         </div>
     </section>
-    <!-- footer Section Ends Here -->
+    <!-- Footer Ends -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
